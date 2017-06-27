caa3d861a4a716631cdbcb7a26b3247aa5352fe6
Erryl Alvares <erryl1@hotmail.com>
2017-06-27 13:43:20 +0530

    Add project files.

---
+++ IncredibleInvoicer/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="IncredibleInvoicer.MvcApplication" Language="C#" %>
---
+++ IncredibleInvoicer/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(IncredibleInvoicer.Startup))]
+namespace IncredibleInvoicer
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
